096ece451bb381a845ee2113af4e527db88b97dd
Abdelaziz <aziznour529@gmail.com>
2025-08-30 23:08:26 +0100

    Add files via upload

---
+++ src/backend/api/header.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('expose_php', 'off');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: SAMEORIGIN');
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+header('X-Frame-Options: DENY');
+header('Referrer-Policy: no-referrer');
+?>
+
+
+
+
